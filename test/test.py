f797825e990fb4b882492816a0e4dd8d496d3e9f
MauriceManning <maurice.manning@gmail.com>
2018-03-09 14:12:37 +0800

    updates

--- test/test.py
+++ test/test.py
@@ -1,17 +1,55 @@
 import pymongo
+import psycopg2
 import logging
+import pprint
 
 from pymongo import MongoClient
 
 logging.basicConfig(filename='example.log',level=logging.INFO)
 
-client = MongoClient()
+client = MongoClient('mongodb://mongodb:27017/')
+db = client['ingestdb']
+db.authenticate('gtt', 'gtt123')
 
-db = client['gtt']
+#list existing collections
+print( 'collections: ' + str(db.collection_names(include_system_collections=False) ) )
+testcollection = db.testcollection
+posts = db.posts
 
-collection = db['test-collection']
+#test insert into mongodb
+post1 = {"foo": "bar"}
+post2 = {"foo2": "bar2"}
+post_id1 = db.testcollection.insert_one(post1).inserted_id
+logging.info('inserted one doc: ' + str(post_id1) )
+print('inserted one doc: ' + str(post_id1) )
 
-post = {"foo": "bar"}
-post_id = db.posts.insert_one(post)
-logging.info('inserted one doc: ' + str(post_id) )
-print('inserted one doc: ' + str(post_id) )
+post_id2 = db.testcollection.insert_one(post2).inserted_id
+logging.info('inserted two doc: ' + str(post_id2) )
+print('inserted two doc: ' + str(post_id2) )
+
+#pp = pprint.PrettyPrinter(width=41, compact=True)
+
+# retrieve all docs in collection
+print('list testcollection')
+for doc in testcollection.find():
+    pprint.pprint( doc )
+
+#print('list posts')
+#for doc in posts.find({}):
+#    pp.pprint( doc )
+
+# test connection to postgres
+try: 
+    import time
+    time.sleep(7)
+    conn = psycopg2.connect(database='accounts', user='gtt', password='password123', host='pgdb')
+    #conn = psycopg2.connect(user='posgres', password='password123', host='pgdb')
+    #cursor = conn.cursor()
+    print('connected to postgres')
+except:
+    print('postgres connect failed.')
+
+print ('end')
+
+    
+
