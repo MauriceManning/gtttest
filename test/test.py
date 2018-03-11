14263bc1ee7226df99f76e1d8a0c4ec7436c811f
Maurice Manning <maurice.manning@gmail.com>
2018-03-11 21:39:50 +0800

    Update test.py

--- test/test.py
+++ test/test.py
@@ -2,54 +2,83 @@ import pymongo
 import psycopg2
 import logging
 import pprint
+from datetime import datetime
+import os
+
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 from pymongo import MongoClient
 
+
+
+
 logging.basicConfig(filename='example.log',level=logging.INFO)
 
-client = MongoClient('mongodb://mongodb:27017/')
-db = client['ingestdb']
-db.authenticate('gtt', 'gtt123')
+def mongoconnect():
+    client = MongoClient('mongodb://mongodb:27017/')
+    db = client['ingestdb']
+    db.authenticate('gtt', 'gtt123')
+    return client, db
 
-#list existing collections
-print( 'collections: ' + str(db.collection_names(include_system_collections=False) ) )
-testcollection = db.testcollection
-posts = db.posts
+def mongolistcollections(db):
+    #list existing collections
+    print( 'collections: ' + str(db.collection_names(include_system_collections=False) ) )
+    testcollection = db.testcollection
+    posts = db.posts
+    
+def mongotestinsert():
+    #test insert into mongodb
+    post1 = {"foo": "bar"}
+    post2 = {"foo2": "bar2"}
+    post_id1 = db.testcollection.insert_one(post1).inserted_id
+    logging.info('inserted one doc: ' + str(post_id1) )
+    print('inserted one doc: ' + str(post_id1) )
 
-#test insert into mongodb
-post1 = {"foo": "bar"}
-post2 = {"foo2": "bar2"}
-post_id1 = db.testcollection.insert_one(post1).inserted_id
-logging.info('inserted one doc: ' + str(post_id1) )
-print('inserted one doc: ' + str(post_id1) )
+    post_id2 = db.testcollection.insert_one(post2).inserted_id
+    logging.info('inserted two doc: ' + str(post_id2) )
+    print('inserted two doc: ' + str(post_id2) )
 
-post_id2 = db.testcollection.insert_one(post2).inserted_id
-logging.info('inserted two doc: ' + str(post_id2) )
-print('inserted two doc: ' + str(post_id2) )
+    #pp = pprint.PrettyPrinter(width=41, compact=True)
+def mongoretrieve():
+    # retrieve all docs in collection
+    print('list testcollection')
+    for doc in testcollection.find():
+        pprint.pprint( doc )
 
-#pp = pprint.PrettyPrinter(width=41, compact=True)
+    #print('list posts')
+    #for doc in posts.find({}):
+    #    pp.pprint( doc )
 
-# retrieve all docs in collection
-print('list testcollection')
-for doc in testcollection.find():
-    pprint.pprint( doc )
-
-#print('list posts')
-#for doc in posts.find({}):
-#    pp.pprint( doc )
-
-# test connection to postgres
-try: 
-    import time
-    time.sleep(7)
-    conn = psycopg2.connect(database='accounts', user='gtt', password='password123', host='pgdb')
-    #conn = psycopg2.connect(user='posgres', password='password123', host='pgdb')
-    #cursor = conn.cursor()
-    print('connected to postgres')
-except:
-    print('postgres connect failed.')
-
-print ('end')
+def postgresconnect():    
+    # test connection to postgres
+    try: 
+        import time
+        time.sleep(7)
+        conn = psycopg2.connect(database='accounts', user='gtt', password='password123', host='pgdb')
+        #conn = psycopg2.connect(user='posgres', password='password123', host='pgdb')
+        #cursor = conn.cursor()
+        print('connected to postgres')
+    except:
+        print('postgres connect failed.')
 
     
+if __name__ == '__main__':
+    
+    # initial mongodb tests
+    mongoclient, mongodb = mongoconnect()
+    mongolistcollections(mongodb)
+    mongotestinsert(mongodb)
+    
+    # test connection to postgres
+    postgresconnect()
+    
+    scheduler = BlockingScheduler()
+    scheduler.add_job(tick, 'interval', seconds=3)
+    print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
 
+    try:
+        scheduler.start()
+        mongoretrieve(mongodb)
+
+    except (KeyboardInterrupt, SystemExit):
+        pass
