25927704c4e6e23128bd2de30b9aec8612c68ba4
Maurice Manning <maurice.manning@gmail.com>
2018-03-11 22:13:51 +0800

    Update pubtest.py

--- publish/pubtest.py
+++ publish/pubtest.py
@@ -11,9 +11,10 @@ import logging
 logging.debug( 'test publish to GnuSocial')
 
 # get the credentials for this gene
-client = UserClient(acctCredentials.consumerkey, acctCredentials.consumersecret, acctCredentials.accesstoken, acctCredentials.accesstokenseecret)
+#client = UserClient(acctCredentials.consumerkey, acctCredentials.consumersecret, acctCredentials.accesstoken, acctCredentials.accesstokenseecret)
+client = UserClient('537143b467af36c43ba066238b88c915', 'e62fd6b54f04b65b4790737b493a23be', acctCredentials.accesstoken, acctCredentials.accesstokenseecret)
 
-response = client.api.statuses.update.post(status=tweet.message)
+response = client.api.statuses.update.post(status='test this publish')
 
 logging.info( 'post_tweet post id: %s', response.data.id)
 print ("response.data.entities : {0}".format(response.data.entities))
